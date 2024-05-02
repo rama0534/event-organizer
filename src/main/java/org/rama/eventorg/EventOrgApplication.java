a22b17021dcca816464dcca8e4026fc04ebc3222
Rama <sadamramanayudu@gmail.com>
2024-05-02 14:48:24 -0500

    Spring Initializr

---
+++ src/main/java/org/rama/eventorg/EventOrgApplication.java
@@ -0,0 +1,13 @@
+package org.rama.eventorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventOrgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rama/eventorg/EventOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rama.eventorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
